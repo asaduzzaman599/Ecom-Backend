8793d66c9bf2073fd27cf216703be2a0c6437218
asad <asaduzzaman@lapasar.com>
2025-09-10 23:36:03 +0600

    fix: user update operation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,24 @@
-import { User } from '@prisma/client';
+import { Role, User } from '@prisma/client';
+import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsEmail()
+  @IsOptional()
   email?: string;
+
+  @IsString()
   phone: string;
+
+  @IsEnum(Role)
   role: User['role'];
+
+  @IsOptional()
+  @IsString()
   image?: string;
 }
